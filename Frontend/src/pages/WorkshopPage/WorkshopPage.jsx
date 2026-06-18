a995197436201f1734221d6e198ac7873a7541a4
Mohammed Minhaj Mahmood <169143028+MuhammadMinhaj229@users.noreply.github.com>
2026-06-18 14:45:05 +0530

    Update WorkshopPage.jsx

--- Frontend/src/pages/WorkshopPage/WorkshopPage.jsx
+++ Frontend/src/pages/WorkshopPage/WorkshopPage.jsx
@@ -480,7 +480,7 @@ function MissionSection() {
                             mb: 4,
                         }}
                     >
-                        IT Consultancy
+                        Carrer Planet
                     </Typography>
 
                     <Divider sx={{ borderColor: "rgba(255,255,255,0.1)", mb: 4 }} />
